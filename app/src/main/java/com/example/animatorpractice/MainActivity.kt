1ac47bfa1f8a982c43002f8c209d510b41819bba
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-29 18:17:04 +0300

    transition simple

--- app/src/main/java/com/example/animatorpractice/MainActivity.kt
+++ app/src/main/java/com/example/animatorpractice/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.animatorpractice
 
 import android.os.Bundle
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var isTextVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOk.setOnClickListener {
+
+            TransitionManager.beginDelayedTransition(vLinearLayout, Slide(Gravity.RIGHT))
+
+            isTextVisible = !isTextVisible
+            tvDemo.visibility = if (isTextVisible) View.GONE else View.VISIBLE
+
+        }
     }
 }
