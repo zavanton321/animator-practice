458d744589dbab23448df07190c13aca8531b2c1
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-30 14:32:02 +0300

    fragments added

--- app/src/main/java/com/example/animatorpractice/detail/HostActivity.kt
+++ app/src/main/java/com/example/animatorpractice/detail/HostActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.animatorpractice.R
 
-class HostActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity(), StartFragment.FragmentNavigator {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
 
         addFragment(savedInstanceState)
+    }
+
+    override fun goToFinishFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fmtContainer, FinishFragment.newInstance())
+            .addToBackStack(null)
+            .commit()
     }
 
     private fun addFragment(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/animatorpractice/detail/StartFragment.kt
+++ app/src/main/java/com/example/animatorpractice/detail/StartFragment.kt
@@ -1,17 +1,31 @@
 package com.example.animatorpractice.detail
 
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.animatorpractice.R
+import kotlinx.android.synthetic.main.fmt_start.*
 
 class StartFragment : Fragment() {
+
+    interface FragmentNavigator {
+
+        fun goToFinishFragment()
+    }
 
     companion object {
 
         fun newInstance(): StartFragment = StartFragment()
+    }
+
+    private var fragmentNavigator: FragmentNavigator? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        fragmentNavigator = requireActivity() as FragmentNavigator
     }
 
     override fun onCreateView(
@@ -20,5 +34,30 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fmt_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvStart.setOnClickListener {
+            fragmentNavigator?.goToFinishFragment()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_start, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.item_finish) {
+            Toast.makeText(requireActivity(), "Finish", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        fragmentNavigator = null
     }
 }
