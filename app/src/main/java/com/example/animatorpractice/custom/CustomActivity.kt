b85d0bd9a432f21716bf3a966aa2633b89e0c391
Anton Zaviyalov <zavanton123@gmail.com>
2019-11-03 13:34:27 +0300

    custom

---
+++ app/src/main/java/com/example/animatorpractice/custom/CustomActivity.kt
@@ -0,0 +1,13 @@
+package com.example.animatorpractice.custom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.animatorpractice.R
+
+class CustomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_start)
+    }
+}
